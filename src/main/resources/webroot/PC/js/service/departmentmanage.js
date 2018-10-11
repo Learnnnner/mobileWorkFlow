920d274d00229318c25655e20861ee538dc85adb
hawking <h617508155@163.com>
2018-10-11 20:19:16 +0800

    aaa

--- src/main/resources/webroot/PC/js/service/departmentmanage.js
+++ src/main/resources/webroot/PC/js/service/departmentmanage.js
@@ -39,12 +39,12 @@ departmentmanage.service = {
                                 }
                             }
                         }
-                            code += '                                </td>\n' +
-                            '                                <td>\n' +
-                            '                                    <button type="button" class="btn btn-success btn-xs detail edit">编辑</button>\n' +
-                            '                                    <button type="button" class="btn btn-danger btn-xs detail delete">删除</button>\n' +
-                            '                                </td>\n' +
-                            '                            </tr>';
+                        code += '                                </td>\n' +
+                        '                                <td>\n' +
+                        '                                    <button type="button" class="btn btn-success btn-xs detail edit">编辑</button>\n' +
+                        '                                    <button type="button" class="btn btn-danger btn-xs detail delete">删除</button>\n' +
+                        '                                </td>\n' +
+                        '                            </tr>';
                         $('#departmentContainer ').append(code)
                     }
                 } else layer.open({
@@ -86,23 +86,51 @@ departmentmanage.eventHandler = {
                 '            </div>\n' +
                 '        </div>\n' +
                 '        <div class="layui-form-item">\n' +
-                '            <label class="layui-form-label">权限</label>\n' +
+                '            <label class="layui-form-label">描述</label>\n' +
                 '            <div class="layui-input-block">\n' +
                 '                <textarea id="description" placeholder="请输入内容" class="layui-textarea">' + tdArr.eq(1).text() + '</textarea>' +
                 '            </div>\n' +
                 '        </div>\n' +
                 '<div class="layui-form-item">\n' +
-                '    <label class="layui-form-label">复选框</label>\n' +
-                '    <div id="authorization" class="layui-input-block">\n' +
-                '      <input id="1" type="checkbox" title="与我相关">\n' +
-                '      <input id="2" type="checkbox" title="流程创建">\n' +
-                '      <input id="3" type="checkbox" title="流程申请">\n' +
-                '      <input id="4" type="checkbox" title="人员管理">\n' +
-                '      <input id="5" type="checkbox" title="组织管理">\n' +
-                '      <input id="6" type="checkbox" title="权限管理">\n' +
-                '    </div>\n' +
+                '    <label class="layui-form-label">权限</label>\n' +
+                '    <div id="authorization" class="layui-input-block">\n';
+            var authArr = [];
+            self.parents('tr').find('span').each(function () {
+                authArr.push($(this).attr('id'));
+            })
+            if(authArr.indexOf('1') < 0) {
+                code += '      <input id="1" type="checkbox" title="与我相关">\n';
+            }else {
+                code += '      <input id="1" type="checkbox" title="与我相关" checked="">\n';
+            }
+            if(authArr.indexOf('2') < 0) {
+                code += '      <input id="2" type="checkbox" title="流程创建">\n';
+            }  else {
+                code += '      <input id="2" type="checkbox" title="流程创建" checked="">\n';
+            }
+            if(authArr.indexOf('3') < 0) {
+                code += '      <input id="3" type="checkbox" title="流程申请">\n';
+            } else {
+                code += '      <input id="3" type="checkbox" title="流程申请" checked="">\n';
+            }
+            if(authArr.indexOf('4') < 0) {
+                code += '      <input id="4" type="checkbox" title="人员管理">\n';
+            } else {
+                code += '      <input id="4" type="checkbox" title="人员管理" checked="">\n';
+            }
+            if(authArr.indexOf('5') < 0) {
+                code += '      <input id="5" type="checkbox" title="组织管理">\n';
+            } else {
+                code += '      <input id="5" type="checkbox" title="组织管理" checked="">\n';
+            }
+            if(authArr.indexOf('6') < 0) {
+                code += '      <input id="6" type="checkbox" title="权限管理">\n';
+            } else {
+                code += '      <input id="6" type="checkbox" title="权限管理" checked="">\n';
+            }
+            code += '    </div>\n' +
                 '  </div></form>'+
-                '<button class="layui-btn" id="submit" style="margin-left: 168px;margin-bottom: 2vw">确定</button>'+
+                '<button class="layui-btn" id="submit" style="margin-left: 218px;margin-bottom: 2vw">确定</button>'+
                 '<script>'+
                 '   layui.use("form", function(){\n' +
                 '                                   var form = layui.form;\n' +
@@ -146,18 +174,19 @@ departmentmanage.eventHandler = {
             layer.confirm('确定修改该部门的信息吗？', {
                 btn: ['确定','取消'] //按钮
             }, function(){
-                var orgid = $('#orgs').val();
+                var node = editself.parents('tr');
+                var id = node.attr('id');
                 var authorization = [];
                 $('#authorization').find('input:checked').each(function () {
                     authorization.push($(this).attr('id'));
                 });
-                url = MW.server + "/editOrgUser"
+                url = MW.server + "/editOrg"
                 var userdata = {
-                    id : $('#id').val(),
-                    name : $('#name').val(),
-                    description : $('#realname').val(),
+                    'id' : id,
+                    'name' : $('#name').val(),
+                    'description' : $('#description').val(),
+                    'authorization': authorization
                 };
-
                 $.ajax({
                     type: "POST",
                     url: url,
@@ -167,22 +196,33 @@ departmentmanage.eventHandler = {
                         if (200 == data.status) {
                             var $tr = editself.parents('tr').empty();
                             var eletr = $tr;
-                            console.log(eletr);
-                            $tr.append(
-                                '                            <td>'+ $('#id').val() +'</td>\n' +
-                                '                            <td>'+ $('#realname').val() +'</td>\n' +
-                                '                            <td>'+ $('#loginname').val() +'</td>\n' +
-                                '                            <td style="display: none">'+ $('#password').val() +'</td>\n' +
-                                '                            <td>'+ $('#telephone').val() +'</td>\n' +
-                                '                            <td>'+ $('#email').val() +'</td>\n' +
-                                '                            <td>'+ $('#orgs').find("option:selected").text() +'</td>\n' +
+                            var code ='                            <td>'+ $('#name').val() +'</td>\n' +
+                                '                            <td>'+ $('#description').val() +'</td>\n' +
+                                '                            <td>';
+                            for(var i = 0; i < authorization.length; ++ i) {
+                                if(authorization[i] == '1') {
+                                    code += '<span id="1" class="label label-info"><i class="iconfont icon-tijiao fa" style="color: #ffffff"></i></span>\n';
+                                } else if(authorization[i] == '2') {
+                                    code += '<span id="2" class="label label-info"><i class="iconfont icon-bianji fa" style="color: #ffffff"></i></span>\n';
+                                } else if(authorization[i] == '3') {
+                                    code += '<span id="3" class="label label-info"><i class="iconfont icon-biaodan fa" style="color: #ffffff"></i></span>\n';
+                                } else if(authorization[i] == '4') {
+                                    code += '<span id="4" class="label label-info"><i class="iconfont icon-guanlikehu fa" style="color: #ffffff"></i></span>\n';
+                                } else if(authorization[i] == '5') {
+                                    code += '<span id="5" class="label label-info"><i class="iconfont icon-zuzhi fa" style="color: #ffffff"></i></span>\n';
+                                } else if(authorization[i] == '6') {
+                                    code += '<span id="6" class="label label-info"><i class="iconfont icon-permissions-user fa" style="color: #ffffff"></i></span>\n';
+                                }
+                            }
+                            code +='</td>\n' +
                                 '                            <td>\n' +
                                 '                                <button type="button" class="btn btn-success btn-xs  edit">编辑</button>\n' +
                                 '                                <button type="button" class="btn btn-danger btn-xs delete">删除</button>\n' +
-                                '                            </td>\n')
-                            layer.msg('用户信息已修改', {icon: 1});
+                                '                            </td>\n';
+                            $tr.append(code);
+                            layer.msg('组织信息已修改', {icon: 1});
                         } else
-                            layer.msg('用户信息修改失败', {icon: 2});
+                            layer.msg('组织信息修改失败', {icon: 2});
                     }, error: function (data) {
                         $.toptip("操作失败!请检查网络情况或与系统管理员联系！", 'error')
                     }
--- src/main/resources/webroot/PC/js/service/wfapply.js
+++ src/main/resources/webroot/PC/js/service/wfapply.js
@@ -4,7 +4,7 @@ $(function () {
 })
 
 var wfapply = {};
-
+var applyform;
 wfapply.service = {
     init: function () {
         this.initControl();
@@ -17,13 +17,12 @@ wfapply.service = {
                 if (200 == data.status) {
                     if(data.formList.length > 0) {
                         for(var i = 0; i < data.formList.length; ++ i) {
-                            code =
-                            $('tbody').append('<tr>\n' +
+                            $('tbody').append('<tr id="' + data.formList[i][0] + '">\n' +
                                 '<td>'+data.formList[i][1]+'</td><td>'+data.formList[i][2]+'</td><td>'+data.formList[i][4]+'</td>\n' +
                                 '<td><button type="button" class="btn btn-primary btn-xs apply">申请</button>\n' +
                                 '</td></tr>')
                         }
-                    }else {
+                    } else {
                         layer.open({
                             title: '异常'
                             ,content: '未查询到相应数据！'
@@ -43,20 +42,204 @@ wfapply.service = {
                 });
             }
         })
-    },
+    }, getData: function () {
+        var eles = $('.layui-form-item');
+        var data = {};
+
+        for(var i = 0 ; i < eles.length; ++ i) {
+            var title =  $(eles[i]).find('.layui-form-label').html();
+            data[title] = [];
+            if($(eles[i]).data('type') == 'radio') {
+                var checkedeles = $(eles[i]).find(':radio:checked');
+                for (var j = 0; j < checkedeles.length; ++ j) {
+                    var opt = $(checkedeles[j]).attr('title');
+                    data[title].push(opt);
+                }
+            }else if($(eles[i]).data('type') == 'checkbox') {
+                var checkedeles = $(eles[i]).find(':checkbox:checked');
+                for (var j = 0; j < checkedeles.length; ++ j) {
+                    var opt = $(checkedeles[j]).attr('title');
+                    data[title].push(opt);
+                }
+            }else if($(eles[i]).data('type') == 'muti') {
+                var textEle =  $(eles[i]).find('textarea');
+                var text = textEle.val();
+                data[title].push(text);
+            }else if($(eles[i]).data('type') == 'city') {
+                var textEle =  $(eles[i]).find('input').val();
+                var reg = new RegExp('/',"g");
+                var text = textEle.replace(reg,' ');
+                data[title].push(text);
+            }else if($(eles[i]).data('type') == 'date') {
+                var textEle =  $(eles[i]).find('input').val();
+                var reg = new RegExp('-',"g");
+                var text = textEle.replace(reg,'/');
+                data[title].push(text);
+            } else {
+                var value = $(eles[i]).find('input').val();
+                data[title].push(value);
+            }
+        }
+        return data;
+    }
 }
 
 wfapply.eventHandler = {
     handleEvents: function () {
-        this.handleEdit();
-    }, handleEdit: function () {
+        this.handleApply();
+        this.handleSubmit();
+    }, handleApply: function () {
         $(document).on('click', '.apply', function () {
-            var test = '<html>test</html>';
-            layer.open({
-                type: 1,
-                skin: 'layui-layer-rim', //加上边框
-                area: ['500px', '300px'], //宽高
-                content: test
+            var self = $(this);
+            applyform = $(this);
+            var id= self.parents('tr').attr('id');
+            var name = self.parents('tr').find('td:first').text();
+
+            if(id != null && id != undefined) {
+                $.ajax({
+                    type: "POST",
+                    url: MW.server + "/fetchFormData",
+                    data: JSON.stringify({id: id}),
+                    dataType: "json",
+                    success: function (data) {
+                        if (200 == data.status) {
+                            if(data.data.length == 1) {
+                                if(data.data[0][2] != undefined && data.data[0][2] != '' && data.data[0][2] != null) {
+                                    var code = '<div style="width: 80%; margin: auto">\n' +
+                                        '    <div style="text-align: center; margin: 2vw">\n' +
+                                        '        <h1>' + name +'</h1>\n' +
+                                        '    </div>' +
+                                        '<form class="layui-form">\n';
+                                    var json = JSON.parse(data.data[0][2]);
+                                    for(var ele in json) {
+                                        var type = json[ele].type;
+                                        switch (type) {
+                                            case 'single':
+                                                code += '  <div class="layui-form-item" data-type="single">\n' +
+                                                    '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                    '    <div class="layui-input-block">\n' +
+                                                    '      <input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="请输入内容" class="layui-input">\n' +
+                                                    '    </div>\n' +
+                                                    '  </div>';
+                                                break;
+                                            case 'muti':
+                                                code += '<div class="layui-form-item layui-form-text" data-type="muti">\n' +
+                                                    '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                    '    <div class="layui-input-block">\n' +
+                                                    '      <textarea placeholder="请输入内容" class="layui-textarea"></textarea>\n' +
+                                                    '    </div>\n' +
+                                                    '  </div>';
+                                                break;
+                                            case 'radio':
+                                                code += '<div class="layui-form-item" data-type="radio">\n' +
+                                                    '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                    '    <div class="layui-input-block">\n';
+                                                for(var i = 0; i < json[ele].options.length; ++ i) {
+                                                    code += '<input type="radio" name="radio" title="' + json[ele].options[i] + '"><br>\n';
+                                                }
+                                                code +='</div></div>'
+                                                break;
+                                            case 'checkbox':
+                                                code += '<div class="layui-form-item" data-type="checkbox">\n' +
+                                                    '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                    '    <div class="layui-input-block">\n' +
+                                                    '      <select>\n';
+                                                for(var i = 0; i < json[ele].options.length; ++ i) {
+                                                    code += '<input type="checkbox" title="' + json[ele].options[i] + '" lay-skin="primary">';
+                                                }
+                                                code +='</select></div></div>'
+                                                break;
+                                            case 'date':
+                                                code +=  '<div class="layui-form-item" data-type="date">\n' +
+                                                        '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                        '<div class="layui-input-block">' +
+                                                        '  <input type="text" class="layui-input date" style="padding-left: 10px" placeholder="请输入日期" >\n' +
+                                                        '</div></div>'+
+                                                        '<script>\n' +
+                                                        '</script>';
+                                                break;
+                                            case 'city':
+                                                code +=  '<div class="layui-form-item" data-type="city">\n' +
+                                                    '    <label class="layui-form-label">' + ele + '</label>\n' +
+                                                    '<div class="layui-input-block">' +
+                                                    '<div style="position: relative;"><!-- container -->\n' +
+                                                    '  <input id="" class="layui-input city-picker" style="padding-left: 10px" readonly type="text">\n' +
+                                                    '</div>'+
+                                                    '</div></div>';
+                                                break;
+                                            case 'number':
+                                                code += '  <div class="layui-form-item" data-type="number">\n' +
+                                                    '    <label class="layui-form-label">'+ ele +'</label>\n' +
+                                                    '    <div class="layui-input-block">\n' +
+                                                    '      <input type="text" name="title" lay-verify="title" autocomplete="off" placeholder="请输入内容" class="layui-input">\n' +
+                                                    '    </div>\n' +
+                                                    '  </div>';
+                                                break;
+                                            case 'picture':
+
+                                                break;
+                                        }
+                                    }
+                                    code += '  </form><button class="layui-btn" id="submit" style="margin-left: 218px;margin-bottom: 2vw">确定</button></div>';
+                                    layer.open({
+                                        type: 1,
+                                        skin: 'layui-layer-rim', //加上边框
+                                        area: ['600px', '480px'], //宽高
+                                        content: code
+                                    });
+                                    layui.use('form', function(){
+                                        var form = layui.form;
+                                        form.render();
+                                    });
+                                    layui.use("laydate", function(){
+                                        var laydate = layui.laydate;
+                                        laydate.render({
+                                            elem: ".date"
+                                        });
+                                    });
+                                    $(".city-picker").citypicker();
+                                }
+
+                            }else {
+                                $.toptip("数据异常", 'error')
+                            }
+
+                        } else $.toptip("数据获取失败", 'error');
+                    }, error: function (data) {
+                        $.toptip("数据获取失败", 'error');
+                    }
+                })
+            }else $.toptip('访问出错', 'error');
+
+        })
+    }, handleSubmit:function () {
+        $(document).on('click', '#submit', function () {
+            layer.confirm('确定提交该申请吗？', {
+                btn: ['确定','取消'] //按钮
+            }, function() {
+                var data = wfapply.service.getData();
+                var timeStamp = new Date().getTime();
+                var templateId= applyform.parents('tr').attr('id');
+                $.ajax({
+                    type: "POST",
+                    url: MW.server + "/saveFormData",
+                    data: JSON.stringify({
+                        data : JSON.stringify(data),
+                        timeStamp: timeStamp,
+                        templateId: templateId
+                    }),
+                    dataType: "json",
+                    success: function (data) {
+                        if (200 == data.status) {
+                            layer.msg('申请成功', {icon: 1});
+                        } else layer.msg('申请失败', {icon: 2});
+                    }, error:
+                        function (data) {
+                            $.alert("操作失败!请检查网络情况或与系统管理员联系！")
+                        }
+                })
+            }, function(){
+
             });
         })
     }
