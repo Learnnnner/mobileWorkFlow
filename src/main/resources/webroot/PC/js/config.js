f9f7b41b9c039565172714b9acdbb13b244ff026
hawking <h617508155@163.com>
2018-10-23 19:50:46 +0800

    aaa

---
+++ src/main/resources/webroot/PC/js/config.js
@@ -0,0 +1,3 @@
+var MW = MW || {};
+MW.server = "http://10.108.224.185:8080";
+// YT.appId = "wxd11d28db394456cf";
---
+++ src/main/resources/webroot/PC/js/login.js
@@ -0,0 +1,48 @@
+$(function () {
+    login.service.init();
+    login.eventHandler.handleEvents();
+})
+
+var  login = {};
+
+login.data = {
+    getData: function () {
+        return {
+            username: $('#username').val(),
+            password: $('#password').val()
+        }
+    }
+}
+
+login.service = {
+    init: function () {
+
+    }, login: function () {
+        var data = login.data.getData();
+        return data.username && "请输入账号" != data.username ? data.password && "请输入密码" != data.username? void $.ajax({
+            type: "POST",
+            url: MW.server + "/login",
+            data: JSON.stringify(data),
+            dataType: "json",
+            success: function (data) {
+                if (200 == data.status) {
+                    var url = MW.server + '/PC/index';
+                    window.location.href = url;
+                } else $.alert("登录失败，请检查用户名或密码是否正确!")
+            }, error: function (data) {
+                    $.alert("操作失败!请检查网络情况或与系统管理员联系！")
+                }
+        }) : void $.alert("请输入密码!") : void $.alert("请输入用户名!")
+    }
+}
+
+
+login.eventHandler = {
+    handleEvents: function () {
+        this.handleLogin();
+    }, handleLogin: function () {
+        $('#submit').click(function () {
+            login.service.login();
+        })
+    }
+}
