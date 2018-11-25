7fbb1394bb0decf1dcde38bdfea96e82a9151f91
hawking <h617508155@163.com>
2018-11-25 20:55:14 +0800

    aaa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import http.HttpServerVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 
 public class Main {
     public static void main(String[] args){
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(MainVerticle.class.getName());
+//        vertx.deployVerticle(MainVerticle.class.getName());
+        vertx.deployVerticle(HttpServerVerticle.class.getName(), new DeploymentOptions()
+                .setInstances(2 * Runtime.getRuntime().availableProcessors()));
+        System.out.println(2 * Runtime.getRuntime().availableProcessors());
     }
 
 }
